cb952b22584ee89e5129ea14d834f9179ca8bd75
Jonathan Gillham <jonathan.gillham@gamil.com>
2014-05-02 02:24:13 +0100

    Added memcache methods with gofmt.

--- mcache.go
+++ mcache.go
@@ -1,153 +1,153 @@
 package mcache
 
 import (
-    "reflect"
-    "appengine"
-    "appengine/memcache"
+	"appengine"
+	"appengine/memcache"
+	"reflect"
 )
 
 var (
-    ErrCacheMiss = memcache.ErrCacheMiss
+	ErrCacheMiss = memcache.ErrCacheMiss
 )
 
 func Add(c appengine.Context, item *memcache.Item) error {
-    return memcache.Add(c, item)
+	return memcache.Add(c, item)
 }
 
 func AddMulti(c appengine.Context, items []*memcache.Item) error {
-    return memcache.AddMulti(c, items)
+	return memcache.AddMulti(c, items)
 }
 
 func CompareAndSwap(c appengine.Context, item *memcache.Item) error {
-    return memcache.CompareAndSwap(c, item)
+	return memcache.CompareAndSwap(c, item)
 }
 
 func CompareAndSwapMulti(c appengine.Context, items []*memcache.Item) error {
-    return memcache.CompareAndSwapMulti(c, items)
+	return memcache.CompareAndSwapMulti(c, items)
 }
 
 func Delete(c appengine.Context, key string) error {
-    return memcache.Delete(c, key)
+	return memcache.Delete(c, key)
 }
 
 func DeleteMulti(c appengine.Context, keys []string) error {
-    return memcache.DeleteMulti(c, keys)
+	return memcache.DeleteMulti(c, keys)
 }
 
 func Flush(c appengine.Context) error {
-    return memcache.Flush(c)
+	return memcache.Flush(c)
 }
 
 func Get(c appengine.Context, key string) (*memcache.Item, error) {
-    return memcache.Get(c, key)
+	return memcache.Get(c, key)
 }
 
 func GetMulti(c appengine.Context, keys []string) ([]*memcache.Item, error) {
-    itemsMap, err := memcache.GetMulti(c, keys)
-    if err != nil {
-        return nil, err
-    }
+	itemsMap, err := memcache.GetMulti(c, keys)
+	if err != nil {
+		return nil, err
+	}
 
-    items := make([]*memcache.Item, len(keys))
-    multiErr, errsNil := make(appengine.MultiError, len(keys)), true
-    for i, key := range keys {
-        if item, ok := itemsMap[key]; ok {
-            items[i] = item
-        } else {
-            multiErr[i] = memcache.ErrCacheMiss
-            errsNil = false
-        }
-    }
-    if errsNil {
-        return items, nil
-    }
-    return items, multiErr
+	items := make([]*memcache.Item, len(keys))
+	multiErr, errsNil := make(appengine.MultiError, len(keys)), true
+	for i, key := range keys {
+		if item, ok := itemsMap[key]; ok {
+			items[i] = item
+		} else {
+			multiErr[i] = memcache.ErrCacheMiss
+			errsNil = false
+		}
+	}
+	if errsNil {
+		return items, nil
+	}
+	return items, multiErr
 }
 
-func Increment(c appengine.Context, 
-    key string, delta int64, initialValue uint64) (newValue uint64, err error) {
-    return memcache.Increment(c, key, delta, initialValue)
+func Increment(c appengine.Context,
+	key string, delta int64, initialValue uint64) (newValue uint64, err error) {
+	return memcache.Increment(c, key, delta, initialValue)
 }
 
-func IncrementExisting(c appengine.Context, 
-    key string, delta int64) (newValue uint64, err error) {
-    return IncrementExisting(c, key, delta) 
+func IncrementExisting(c appengine.Context,
+	key string, delta int64) (newValue uint64, err error) {
+	return IncrementExisting(c, key, delta)
 }
 
 func Set(c appengine.Context, item *memcache.Item) error {
-    return memcache.Set(c, item)
+	return memcache.Set(c, item)
 }
 
 func SetMulti(c appengine.Context, items []*memcache.Item) error {
-    return memcache.SetMulti(c, items)
+	return memcache.SetMulti(c, items)
 }
 
 type Codec struct {
-//    Marshal func(interface{}) ([]byte, error)
- //   Unmarshal func([]byte, interface{}) error
-    memcache.Codec
+	//    Marshal func(interface{}) ([]byte, error)
+	//   Unmarshal func([]byte, interface{}) error
+	memcache.Codec
 }
 
-func (cd Codec) GetMulti(c appengine.Context, keys []string, 
-    dst interface{}) ([]*memcache.Item, error) {
+func (cd Codec) GetMulti(c appengine.Context, keys []string,
+	dst interface{}) ([]*memcache.Item, error) {
 
-    itemsMap, err := memcache.GetMulti(c, keys)
-    if err != nil {
-        return nil, err
-    }
+	itemsMap, err := memcache.GetMulti(c, keys)
+	if err != nil {
+		return nil, err
+	}
 
-    v := reflect.ValueOf(dst)
-    items := make([]*memcache.Item, len(keys))
-    multiErr, errsNil := make(appengine.MultiError, len(keys)), true
-    for i, key := range keys {
-        if item, ok := itemsMap[key]; ok {
-            err := cd.Unmarshal(item.Value, v.Index(i).Interface())
-            if err != nil {
-                return nil, err
-            }
-            items[i] = item
-        } else {
-            multiErr[i] = memcache.ErrCacheMiss
-            errsNil = false
-        }
-    }
-    if errsNil {
-        return items, nil
-    }
-    return items, multiErr
+	v := reflect.ValueOf(dst)
+	items := make([]*memcache.Item, len(keys))
+	multiErr, errsNil := make(appengine.MultiError, len(keys)), true
+	for i, key := range keys {
+		if item, ok := itemsMap[key]; ok {
+			err := cd.Unmarshal(item.Value, v.Index(i).Interface())
+			if err != nil {
+				return nil, err
+			}
+			items[i] = item
+		} else {
+			multiErr[i] = memcache.ErrCacheMiss
+			errsNil = false
+		}
+	}
+	if errsNil {
+		return items, nil
+	}
+	return items, multiErr
 }
 
-func (cd Codec) Get(c appengine.Context, 
-    key string, dst interface{}) (*memcache.Item, error) {
-    return cd.Codec.Get(c, key, dst)
+func (cd Codec) Get(c appengine.Context,
+	key string, dst interface{}) (*memcache.Item, error) {
+	return cd.Codec.Get(c, key, dst)
 }
 
 func (cd Codec) Set(c appengine.Context, item *memcache.Item) error {
-    return cd.Codec.Set(c, item)
+	return cd.Codec.Set(c, item)
 }
 
 func (cd Codec) SetMulti(c appengine.Context, items []*memcache.Item) error {
-    return cd.Codec.SetMulti(c, items)
+	return cd.Codec.SetMulti(c, items)
 }
 
 func (cd Codec) Add(c appengine.Context, item *memcache.Item) error {
-    return cd.Codec.Add(c, item)
+	return cd.Codec.Add(c, item)
 }
 
 func (cd Codec) AddMulti(c appengine.Context, items []*memcache.Item) error {
-    return cd.Codec.AddMulti(c, items)
+	return cd.Codec.AddMulti(c, items)
 }
 
 func (cd Codec) CompareAndSwap(c appengine.Context, item *memcache.Item) error {
-    return cd.Codec.CompareAndSwap(c, item)
+	return cd.Codec.CompareAndSwap(c, item)
 }
 
-func (cd Codec) CompareAndSwapMulti(c appengine.Context, 
-    items []*memcache.Item) error {
-    return cd.Codec.CompareAndSwapMulti(c, items)
+func (cd Codec) CompareAndSwapMulti(c appengine.Context,
+	items []*memcache.Item) error {
+	return cd.Codec.CompareAndSwapMulti(c, items)
 }
 
 var (
-    Gob = Codec{Codec:memcache.Gob}
-    JSON = Codec{Codec: memcache.JSON}
+	Gob  = Codec{Codec: memcache.Gob}
+	JSON = Codec{Codec: memcache.JSON}
 )
--- mcache_test.go
+++ mcache_test.go
@@ -1,126 +1,125 @@
 package mcache_test
 
 import (
-    "github.com/qedus/mcache"
-    "appengine"
-    "bytes"
-    "appengine/aetest"
-    "appengine/memcache"
-    "testing"
+	"appengine"
+	"appengine/aetest"
+	"appengine/memcache"
+	"bytes"
+	"github.com/qedus/mcache"
+	"testing"
 )
 
 func TestMcache(t *testing.T) {
-    c, err := aetest.NewContext(nil)
-    if err != nil {
-        t.Fatal(err)
-    }
-    defer c.Close()
+	c, err := aetest.NewContext(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
 
-    item, err := mcache.Get(c, "one")
-    if err != memcache.ErrCacheMiss {
-        t.Fatalf("incorrect error: %s", err)
-    }
-    if item != nil {
-        t.Fatal("item should be nil")
-    }
+	item, err := mcache.Get(c, "one")
+	if err != memcache.ErrCacheMiss {
+		t.Fatalf("incorrect error: %s", err)
+	}
+	if item != nil {
+		t.Fatal("item should be nil")
+	}
 
-    err = mcache.Add(c, &memcache.Item{
-        Key: "one",
-        Value: []byte("oneval"),})
-    if err != nil {
-        t.Fatal(err)
-    }
-    err = mcache.Add(c, &memcache.Item{
-        Key: "two",
-        Value: []byte("twoval"),})
-    if err != nil {
-        t.Fatal(err)
-    }
+	err = mcache.Add(c, &memcache.Item{
+		Key:   "one",
+		Value: []byte("oneval")})
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = mcache.Add(c, &memcache.Item{
+		Key:   "two",
+		Value: []byte("twoval")})
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    // Test nil error.
-    keys := []string{"one", "two"}
-    items, err := mcache.GetMulti(c, keys)
-    if err != nil {
-        t.Fatal(err)
-    }
-    for i, key := range keys {
-        item := items[i]
-        if key != item.Key {
-            t.Fatal("incorrect key")
-        }
-        val := []byte(key + "val")
-        if !bytes.Equal(item.Value, val) {
-            t.Fatal("incorrect val")
-        }
-    }
+	// Test nil error.
+	keys := []string{"one", "two"}
+	items, err := mcache.GetMulti(c, keys)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i, key := range keys {
+		item := items[i]
+		if key != item.Key {
+			t.Fatal("incorrect key")
+		}
+		val := []byte(key + "val")
+		if !bytes.Equal(item.Value, val) {
+			t.Fatal("incorrect val")
+		}
+	}
 
-    // Test cache miss error.
-    keys = []string{"one", "onehalf", "two"}
-    items, err = mcache.GetMulti(c, keys)
-    me, ok := err.(appengine.MultiError)
-    if !ok {
-        t.Fatal("not appengine.MultiError")
-    }
-    for i, key := range keys {
-        if i == 1 {
-            if me[i] != mcache.ErrCacheMiss {
-                t.Fatal("incorrect error")
-            }
-            continue
-        }
-        if me[i] != nil {
-            t.Fatal("not nil error")
-        }
-        item := items[i]
-        if key != item.Key {
-            t.Fatal("incorrect key")
-        }
-        val := []byte(key + "val")
-        if !bytes.Equal(item.Value, val) {
-            t.Fatal("incorrect val")
-        }
-    }
+	// Test cache miss error.
+	keys = []string{"one", "onehalf", "two"}
+	items, err = mcache.GetMulti(c, keys)
+	me, ok := err.(appengine.MultiError)
+	if !ok {
+		t.Fatal("not appengine.MultiError")
+	}
+	for i, key := range keys {
+		if i == 1 {
+			if me[i] != mcache.ErrCacheMiss {
+				t.Fatal("incorrect error")
+			}
+			continue
+		}
+		if me[i] != nil {
+			t.Fatal("not nil error")
+		}
+		item := items[i]
+		if key != item.Key {
+			t.Fatal("incorrect key")
+		}
+		val := []byte(key + "val")
+		if !bytes.Equal(item.Value, val) {
+			t.Fatal("incorrect val")
+		}
+	}
 
 }
 
 func TestMcacheCodec(t *testing.T) {
-    c, err := aetest.NewContext(nil)
-    if err != nil {
-        t.Fatal(err)
-    }
-    defer c.Close()
+	c, err := aetest.NewContext(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
 
-    type Entity struct {
-        Value int
-    }
+	type Entity struct {
+		Value int
+	}
 
-    keys := []string{"0", "1"}
-    for i, key := range keys {
-        err = mcache.Gob.Add(c, &memcache.Item{
-            Key: key,
-            Object: &Entity{i},
-        })
-        if err != nil {
-            t.Fatal(err)
-        }
-    }
+	keys := []string{"0", "1"}
+	for i, key := range keys {
+		err = mcache.Gob.Add(c, &memcache.Item{
+			Key:    key,
+			Object: &Entity{i},
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
 
-    entities := make([]*Entity, len(keys))
-    for i := range entities {
-        entities[i] = &Entity{}
-    }
-    items, err := mcache.Gob.GetMulti(c, keys, entities)
-    if err != nil {
-        t.Fatal(err)
-    }
+	entities := make([]*Entity, len(keys))
+	for i := range entities {
+		entities[i] = &Entity{}
+	}
+	items, err := mcache.Gob.GetMulti(c, keys, entities)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    for i, key := range keys {
-        if entities[i].Value != i {
-            t.Fatal("incorrect value")
-        }
-        if items[i].Key != key {
-            t.Fatal("incorrect key")
-        }
-    }
+	for i, key := range keys {
+		if entities[i].Value != i {
+			t.Fatal("incorrect value")
+		}
+		if items[i].Key != key {
+			t.Fatal("incorrect key")
+		}
+	}
 }
-
